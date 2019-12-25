415c86cacf8586cd3330b5c74a7638835eebb78a
Jacek Bogdański <stuff.jackozbogdanca@gmail.com>
2019-12-25 23:43:45 +0100

    feat: Styling

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-alert("Dziękujemy za odwiedzenie strony. Życzymy dowidzenia.");
+/* alert("Dziękujemy za odwiedzenie strony. Życzymy dowidzenia.");
 const name = "Jacek Bogdanski";
 const age = 17;
 console.log(`Jestem ${name} i mam ${age} lat.`);
+
+*/
