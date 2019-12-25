cadd14106fdf36e99ee07686e929e9fe69d6b1ed
Jacek Bogdański <stuff.jackozbogdanca@gmail.com>
2019-12-25 23:08:18 +0100

    feat: Getting ordered

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Twój komputer został zainfekowany. Miłego dnia.");
+alert("Dziękujemy za odwiedzenie strony. Życzymy dowidzenia.");
 const name = "Jacek Bogdanski";
 const age = 17;
 console.log(`Jestem ${name} i mam ${age} lat.`);
