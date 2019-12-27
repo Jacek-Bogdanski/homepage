9e00c65569e65110d7d4379ecfd813f465bd100f
Jacek Bogdański <stuff.jackozbogdanca@gmail.com>
2019-12-27 15:09:36 +0100

    feat: bem structure

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 /* alert("Dziękujemy za odwiedzenie strony. Życzymy dowidzenia.");
+ */
 const name = "Jacek Bogdanski";
 const age = 17;
 console.log(`Jestem ${name} i mam ${age} lat.`);
 
-*/
+const heading = document.querySelector('.heading');
+console.log(heading);
+
+const news = document.querySelector('.news--js');
+news.innerHTML = "Wiadomość: Lorem Ipsum dolor sit amet :) ";
